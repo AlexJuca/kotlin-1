311c6ef209eb1245fda92aef6626574676f61a5a
vldf <vladislavf7@gmail.com>
2020-08-18 11:33:27 +0300

    [FIR] Refactoring for extended checkers

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/extended/ArrayEqualityCanBeReplacedWithEquals.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/extended/ArrayEqualityCanBeReplacedWithEquals.kt
@@ -20,15 +20,15 @@ import org.jetbrains.kotlin.fir.types.classId
 import org.jetbrains.kotlin.fir.types.coneType
 
 object ArrayEqualityCanBeReplacedWithEquals : FirBasicExpresionChecker() {
-    override fun check(equality: FirStatement, context: CheckerContext, reporter: DiagnosticReporter) {
-        if (equality !is FirEqualityOperatorCall) return
-        if (equality.operation != FirOperation.EQ && equality.operation != FirOperation.NOT_EQ) return
-        val left = equality.arguments.getOrNull(0) ?: return
-        val right = equality.arguments.getOrNull(1) ?: return
+    override fun check(expression: FirStatement, context: CheckerContext, reporter: DiagnosticReporter) {
+        if (expression !is FirEqualityOperatorCall) return
+        if (expression.operation != FirOperation.EQ && expression.operation != FirOperation.NOT_EQ) return
+        val left = expression.arguments.getOrNull(0) ?: return
+        val right = expression.arguments.getOrNull(1) ?: return
 
         if (left.typeRef.coneType.classId != StandardClassIds.Array) return
         if (right.typeRef.coneType.classId != StandardClassIds.Array) return
 
-        reporter.report(equality.psi?.children?.get(1)?.toFirPsiSourceElement(), ARRAY_EQUALITY_OPERATOR_CAN_BE_REPLACED_WITH_EQUALS)
+        reporter.report(expression.psi?.children?.get(1)?.toFirPsiSourceElement(), ARRAY_EQUALITY_OPERATOR_CAN_BE_REPLACED_WITH_EQUALS)
     }
 }
